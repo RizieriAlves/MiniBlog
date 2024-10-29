efa189b4b4b3be745ba979b538e70d47a64e9ea2
RizieriAlves <viniciusrizieri@hotmail.com>
2024-10-29 11:10:16 -0300

    upd

---
+++ src/firebase/config.js
@@ -0,0 +1,18 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+const db = getFirestore(app);
+
+export { db, app };
